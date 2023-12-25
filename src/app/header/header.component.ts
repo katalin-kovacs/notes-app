94ee841437689beaebeade8f3c8c3286925d2c96
Katalin Kovács <kovacs.katalin89@gmail.com>
2023-12-25 18:55:38 +0100

    add header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  template: ` <h1>My header</h1> `,
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
